1dbb152c559be31e81cfc041ee76e3dc3d422859
Ratimir <ra0953029364@gmail.com>
2019-12-21 15:41:55 +0200

    Добавьте файлы проекта.

---
+++ inheritance_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace inheritance_1
+{
+    class Printer
+    {
+        protected ConsoleColor Color;
+
+        public Printer()
+        {
+            Color = ConsoleColor.Blue;
+        }
+
+        public Printer(ConsoleColor color)
+        {
+            Color = color;
+        }
+
+        public virtual void Print(string value)
+        {
+            Console.ForegroundColor = this.Color;
+            Console.WriteLine(value);
+        }
+    }
+    class MyPrinter : Printer
+    {
+        public MyPrinter()
+        {
+            Color = ConsoleColor.DarkGreen;
+        }
+
+        public MyPrinter(ConsoleColor color) : base(color)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p1 = new Printer();
+            Printer p2 = new MyPrinter();
+            MyPrinter p3 = new MyPrinter();
+            Printer p4 = new Printer(ConsoleColor.DarkRed);
+            p1.Print("Printer");
+            p2.Print("Printer as MyPrinter");
+            p3.Print("MyPrinter");
+            p4.Print("Color=DarkRed");
+            Console.ReadKey();
+        }
+    }
+}
